e6665adcdf803c457c5ce9a7145b2027e18018b0
1bthomp <brianthompson080@gmail.com>
2022-03-05 15:32:21 -0500

    Second Commit

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -1,37 +1,83 @@
 package com.example.myloginapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 
 public class MainActivity extends AppCompatActivity {
+    //Google//
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
+        googleBtn = findViewById(R.id.google_btn);
 
-        MaterialButton loginbtn = findViewById(R.id.loginbtn);
-        //user and password: admin //
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            navigateToSecondActivity();
+        }
+
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && (password.getText()).toString().equals("admin")) {
-                    //Correct Password
-                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                } else
-                    //Incorrect
-                    Toast.makeText(MainActivity.this, "LOGIN FAILED!!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                signIn();
             }
         });
+
+
+
     }
-}
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }}
+
+
+
+
+
+
+
